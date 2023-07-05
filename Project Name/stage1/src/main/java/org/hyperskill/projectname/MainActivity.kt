b334e5eec669a0ad52203cf6160ae8e31785f549
RedJocker <mauricio.guaruja@gmail.com>
2023-07-05 15:53:23 -0300

    add warn comment on test limitations related to static variables

--- Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
+++ Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+            Tests for android can not guarantee the correctness of solutions that make use of
+            mutation on "static" variables. You should avoid using those.
+            Consider "static" as being anything on kotlin that is transpiled to java
+            into a static variable. That includes global variables and variables inside
+            singletons declared with keyword object, including companion object.
+            This limitation is related to the use of JUnit on tests. JUnit re-instantiate all
+            instance variable for each test method, but it does not re-instantiate static variables.
+            The use of static variable to hold state can lead to state from one test to spill over
+            to another test and cause unexpected results.
+            Using mutation on static variables is considered a bad practice anyway and no measure
+            attempting to give support to that pattern will be made.
+         */
     }
 }
