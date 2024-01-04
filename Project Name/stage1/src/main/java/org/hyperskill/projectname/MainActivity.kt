2e8c5fb7d8ca43b326b5adb36c7c854d3eee3457
RedJocker <64465262+RedJocker@users.noreply.github.com>
2024-01-04 15:30:21 -0300

    Update test warning

--- Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
+++ Project Name/stage1/src/main/java/org/hyperskill/projectname/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         /*
             Tests for android can not guarantee the correctness of solutions that make use of
-            mutation on "static" variables. You should avoid using those.
+            mutation on "static" variables to keep state. You should avoid using those.
             Consider "static" as being anything on kotlin that is transpiled to java
             into a static variable. That includes global variables and variables inside
             singletons declared with keyword object, including companion object.
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
             instance variable for each test method, but it does not re-instantiate static variables.
             The use of static variable to hold state can lead to state from one test to spill over
             to another test and cause unexpected results.
-            Using mutation on static variables is considered a bad practice anyway and no measure
+            Using mutation on static variables to keep state 
+            is considered a bad practice anyway and no measure
             attempting to give support to that pattern will be made.
          */
     }
